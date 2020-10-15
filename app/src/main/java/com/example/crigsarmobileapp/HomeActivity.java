97ed0807f0c1f36246f912425966c1f5f32b6377
alexandreb <alexandreb@harrelmallac.com>
2020-10-15 17:06:56 +0400

    Home page list view

--- app/src/main/java/com/example/crigsarmobileapp/HomeActivity.java
+++ app/src/main/java/com/example/crigsarmobileapp/HomeActivity.java
@@ -1,14 +1,61 @@
 package com.example.crigsarmobileapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ListView lvHome;
+    int rImag[] = {R.drawable.crigs_logo, R.drawable.hmt_logo, R.drawable.crigs_logo, R.drawable.hmt_logo, R.drawable.crigs_logo};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        lvHome = findViewById(R.id.lvHome);
+        myAdapter adapter = new myAdapter(this, rImag);
+        lvHome.setAdapter(adapter);
+}
+
+    class myAdapter extends ArrayAdapter<String>{
+        Context context;
+        int myImages[];
+
+
+        public myAdapter(@NonNull Context c, int myImages[]) {
+            super(c, R.layout.home_page_list_view_layout, R.id.img);
+            this.context = c;
+            this.myImages = myImages;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @NonNull View convertView, @NonNull ViewGroup parent){
+            //LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(context.LAYOUT_INFLATER_SERVICE);
+
+            //View row = layoutInflater.inflate(R.layout.home_page_list_view_layout, parent, false);
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.home_page_list_view_layout, null, true);
+            ImageView img = findViewById(R.id.img);
+
+            img.setImageResource(rImag[position]);
+
+            return view;
+        }
     }
 }
--- app/src/main/java/com/example/crigsarmobileapp/LoginActivity.java
+++ app/src/main/java/com/example/crigsarmobileapp/LoginActivity.java
@@ -3,9 +3,12 @@ package com.example.crigsarmobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,5 +23,14 @@ public class LoginActivity extends AppCompatActivity {
 
         ConstraintLayout constraintLayout = findViewById(R.id.loginCons);
         constraintLayout.setAnimation(popUpAnim);
+
+        Button butLogin = findViewById(R.id.butLogin);
+        butLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
